46a94bf94eb2125b774e445f852dc794d437dc23
Raghu Betina <raghu@firstdraft.com>
2018-04-04 15:24:22 +0000

    Direct association between photos and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :photo
+
   belongs_to :commentor,
              :class_name => "User",
              :foreign_key => "user_id"
